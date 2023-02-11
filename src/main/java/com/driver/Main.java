959e517463b3768cffe7010ff83c967b5c11ea92
SUMAN SAURABH <110984246+Git-suman309@users.noreply.github.com>
2023-02-11 19:51:20 +0530

    encapsulation soln

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        RWOnly o = new RWOnly();
+
+        o.setName("xyz");
+        o.getName();
+    }
   
 }
